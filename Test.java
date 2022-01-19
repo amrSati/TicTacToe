391e663d69481de8badd5347a9a3387af356abfc
Amro Sati <amrsatti98@gmail.com>
2022-01-19 02:20:58 +0200

    first commit

--- Test.java
+++ Test.java
@@ -1,7 +1,5 @@
 public class Test {
-    private static final String HI = "HI";
-
-    public static void main(String[] args) {
-        System.out.println(HI);
+        public static void main(String[] args) {
+        System.out.println("HI");
     }
 }
